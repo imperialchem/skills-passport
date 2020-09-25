a95a1d522760a721b5998647783a3177cd7083b2
David <David>
2020-09-25 18:35:51 +0100

    Added profile

--- chemtrack_app/models.py
+++ chemtrack_app/models.py
@@ -1,10 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import datetime
 
 class Assignment(models.Model):
     student_cid = models.CharField(max_length=15)
     teacher_cid = models.CharField(max_length=15)
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cid = models.TextField(max_length=15, blank=True, default="")
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 class Category_template(models.Model):
     name = models.CharField(max_length=200, default="")
@@ -25,7 +40,7 @@ class Descriptor_template(models.Model):
 class Record(models.Model):
     date_creation = models.DateTimeField(auto_now_add=True)
     student_cid = models.CharField(max_length=15)
-    date = models.DateTimeField()
+    date = models.DateField(default=datetime.date.today)
     state = models.IntegerField(default=0)
     levels = models.CharField(max_length=200)
     name = models.CharField(max_length=100, default="")
