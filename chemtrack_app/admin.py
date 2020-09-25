e6dbc411a6afbcde099ab90ef5da13741491b911
David <David>
2020-09-25 18:35:31 +0100

    Added Profile to admin list of tables

--- chemtrack_app/admin.py
+++ chemtrack_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Assignment, Category_template, Descriptor_template, Record, Record_category, Record_descriptor
+from .models import Assignment, Category_template, Descriptor_template, Record, Record_category, Record_descriptor, Profile
 
 admin.site.register(Assignment)
 admin.site.register(Category_template)
@@ -8,3 +8,4 @@ admin.site.register(Descriptor_template)
 admin.site.register(Record)
 admin.site.register(Record_category)
 admin.site.register(Record_descriptor)
+admin.site.register(Profile)
