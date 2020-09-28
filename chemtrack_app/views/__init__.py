e1fd334af1b2f980e16ee1394b0abb36b3b5ad8f
David <David>
2020-09-28 11:04:12 +0100

    added view draft to init

--- chemtrack_app/views/__init__.py
+++ chemtrack_app/views/__init__.py
@@ -1,5 +1,6 @@
 from .index import index
 from .new_draft import create_draft
 from .student_dafts_view import student_dafts_view
+from .view_draft import view_draft
 
 from .check_template import check_template
